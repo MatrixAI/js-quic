75fc2949b240531ed42289d05722dd133ce8ab1b
Roger Qiu <roger.qiu@polyhack.io>
2023-05-17 19:14:15 +1000

    fix: enabled `brew update` in order to get the latest rust for macos in `./scripts/brew-install.sh`

--- scripts/brew-install.sh
+++ scripts/brew-install.sh
@@ -10,6 +10,7 @@ export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=1
 export HOMEBREW_NO_AUTO_UPDATE=1
 export HOMEBREW_NO_ANALYTICS=1
 
+brew update
 brew install node@18
 brew link --overwrite node@18
 brew install rust
