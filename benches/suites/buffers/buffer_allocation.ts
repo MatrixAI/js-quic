e99960c0dd3d158314fdb9981fdb5684e5f16c28
Brian Botha <brian.botha@matrix.ai>
2023-09-25 18:20:31 +1000

    wip: removed stub
    [ci skip]

--- benches/suites/buffers/buffer_allocation.ts
+++ benches/suites/buffers/buffer_allocation.ts
@@ -2,7 +2,6 @@ import b from 'benny';
 import { summaryName, suiteCommon } from '../../utils';
 
 async function main() {
-  return;
   const summary = await b.suite(
     summaryName(__filename),
     b.add('Buffer.alloc', () => {
